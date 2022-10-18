99a22a4b96fab552aaf6be67bbed2667595c8720
smmartin1 <smarie333m@gmail.com>
2022-10-18 13:59:22 -0400

    Added CSS style

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
   styleUrls: ['./user-login-form.component.scss']
 })
+
 export class UserLoginFormComponent implements OnInit {
   @Input() userData = {
     Username: '',
@@ -17,22 +19,24 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router
   ) {}
 
   ngOnInit(): void {}
 
   loginUser(): void {
-    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
-     this.dialogRef.close();
-     console.log(result);
-     localStorage.setItem('token', result.token);
-     localStorage.setItem('user', result.users.Username);
-     this.snackBar.open(result, 'OK', {duration: 2000});
-    }, (result) => {
-      console.log(result);
-
-      this.snackBar.open(result, 'OK', {duration: 2000});
-    });
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (result) => {
+         this.dialogRef.close();
+         console.log(result);
+         localStorage.setItem('token', result.token);
+         localStorage.setItem('user', result.users.Username);
+         this.snackBar.open(result, 'OK', {duration: 2000});
+         this.router.navigate(['movies']);
+       }, (result) => {
+        console.log(result);
+        this.snackBar.open(result, 'OK', {duration: 2000});
+      });
   }
 }
